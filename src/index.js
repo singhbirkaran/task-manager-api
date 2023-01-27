14e3e1b28c54d3e85c95878051119b7748d6fd36
singhbirkaran <birkaransingh7@gmail.com>
2023-01-27 23:00:01 +0530

    added app.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-const express=require('express')
-require('./db/mongoose.js')
-const userRouter=require('./routers/user')
-const taskRouter=require('./routers/task')
+const app = require('./app')
 
-const app = express()
 const port = process.env.PORT
 
 // app.use((req,res,next) => {
 //     res.status(503).send('Site under maintenance')
 // })
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server up on port '+port)})
